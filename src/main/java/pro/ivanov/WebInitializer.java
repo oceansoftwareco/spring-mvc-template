cb648133ef17aae937c75b0745b7affb56ca1d8a
Ivan Ivanov <me@ivanov.pro>
2022-10-16 21:03:58 +0300

    update config

--- src/main/java/pro/ivanov/WebInitializer.java
+++ src/main/java/pro/ivanov/WebInitializer.java
@@ -2,37 +2,25 @@ package pro.ivanov;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-  
+
 public class WebInitializer implements WebApplicationInitializer {
-    
-	/*@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
-        ctx.register(WebInitializer.class);  
-        ctx.setServletContext(servletContext);    
-        Dynamic servlet = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(ctx));
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/*");  
-        
-	}*/
 	
 	 @Override
-	 public void onStartup(ServletContext container) {
+	 public void onStartup(ServletContext container) throws ServletException {
 		 AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
 	     
 		 ctx.register(WebInitializer.class);  
 	     ctx.setServletContext(container);
 	     ctx.setConfigLocation("pro.ivanov");   
 	     
-		 ServletRegistration.Dynamic registration = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+		 Dynamic registration = container.addServlet("DispatcherServlet", new DispatcherServlet(ctx));
 		 
 	     registration.setLoadOnStartup(1);
-	     registration.addMapping("/*");
+	     registration.addMapping("/");
 	 }
 }
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<h1>Hello word</h1>
 </body>
 </html>
