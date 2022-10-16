d5c20bcfbb80aacc2c750042ef127576647ab46f
Ivan Ivanov <me@ivanov.pro>
2022-10-16 21:42:16 +0300

    replace Java EE with Jakarta

--- src/main/java/pro/ivanov/WebInitializer.java
+++ src/main/java/pro/ivanov/WebInitializer.java
@@ -1,10 +1,17 @@
 package pro.ivanov;
 
+/*
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
+*/
 
 import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
