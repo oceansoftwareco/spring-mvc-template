8176b8353c3207a706fc796aed70a50e4f8893a8
Ivan Ivanov <me@ivanov.pro>
2022-12-30 18:25:39 +0200

    update spring mvc version and jakarta

--- src/main/java/pro/ivanov/WebConfig.java
+++ src/main/java/pro/ivanov/WebConfig.java
@@ -6,19 +6,28 @@ import org.springframework.context.annotation.ComponentScan;
 
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan("pro.ivanov")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		
+
 		resolver.setPrefix("/WEB-INF/");
 		resolver.setSuffix(".jsp");
-			
+
 		return resolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+				.addResourceHandler("/resources/**")
+				.addResourceLocations("/WEB-INF/resources/*");
 	}
 }
